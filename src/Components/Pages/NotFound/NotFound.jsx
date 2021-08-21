6fcaae3c9ae76414a65ce2796e41d90b6474b2b8
Frostyezz <86774480+Frostyezz@users.noreply.github.com>
2021-08-21 15:38:59 +0300

    Add files via upload

---
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import classes from './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="container my-5 py-1 text-center">
+            <h1 className={classes.title}>404</h1>
+            <h2>OPPS! PAGE NOT FOUND</h2>
+            <p className="lead py-3">Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarity unavailable.</p>
+            <Link className={classes.btn} to='/'>BACK TO HOME PAGE</Link>
+        </div>
+    )
+}
+
+export default NotFound;
